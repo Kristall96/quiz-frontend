cc2065a3606750e58601748497ca9784f92eba1b
Erkan Shakir <erkandev@outlook.com>
2025-01-29 15:31:49 +0200

    testing urls

--- JS/sideRanking.js
+++ JS/sideRanking.js
@@ -1,5 +1,5 @@
-const API_BASE_URL = "https://quiz-backend-rdcd.onrender.com";
 document.addEventListener("DOMContentLoaded", async () => {
+  const API_BASE_URL = "https://quiz-backend-rdcd.onrender.com";
   try {
     const response = await fetch(`${API_BASE_URL}/api/users/rank`, {
       method: "GET",
